27b598a34caa4bbff85794bff7ef79724c10f87e
Tursku <eetu@turakainen.com>
2022-02-17 16:02:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
+import { getCity } from './weather.js'
+import { getWeather } from './weather.js'
+
 
 function App() {
+
+  const apiKey = 'Yj7PbGaTqMeArQC7KArjGRQ1u4b2bXdl'
+  const [weatherData, setWeatherData] = useState("")
+  const [city, setCity] = useState("")
+
+
+  const getWeather = (event) => {
+    if (event.key = "Enter") {
+      fetch(`http://dataservice.accuweather.com/currentconditions/v1/133328?apikey=${apiKey}`).then(
+        response => response.json()
+      ).then(
+        data => {
+          setWeatherData(data)
+        }
+      )
+      
+    }
+  }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        className='input'
+        placeholder='Enter city...'
+        onChange={e => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
+
+      />
+
+      {typeof weatherData.main == 'undefined' ? (
+        <div>
+          <p>Täs o ny sää-appi</p>
+      )}
+
     </div>
   );
 }
