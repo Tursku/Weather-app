81e5ca7b6838715ddd64ef17d1bbd44f1a2e524a
Tursku <eetu@turakainen.com>
2022-02-25 01:38:09 +0200

    changed the API provider and did some major changes

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const apiKey = '60rYAme9Ak25tjyihR3ll0u1CzH1LbSl'
-  const [locationKey, setLocationKey] = useState("")
+  const apiKey = '94a7c2e9db8671f07ef4bc377004a4f8'
   const [weatherData, setWeatherData] = useState("")
   const [city, setCity] = useState("")
-  let startMessage = "Enter any city and press Enter!"
+  let message = "Enter any city and press Enter!"
 
   
   const getWeather = (event) => {
       if (event.key === "Enter") {
-        fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`)
-          .then(response => response.json()).then(data => {
-            setLocationKey(data);
-          });
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`).then(
+          response => response.json()
+        ).then(
+          data => {
+            setWeatherData(data)
+          })
 
-        fetch(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey[0].Key}?apikey=${apiKey}`)
-          .then(response => response.json()).then(data => {
-            setWeatherData(data).then(startMessage = "working!!!")
-          }); 
     }
+    if (weatherData.cod !== "200") {
+      alert(`${weatherData.message}: ${city}`)
     }
+  }
   return (
     <div className="container">
       <input
@@ -33,18 +33,18 @@ function App() {
         onKeyPress={getWeather}
       />
 
-      {typeof weatherData[0] === 'undefined' ? (
+      {typeof weatherData.main === 'undefined' ? (
         <div>
-          <p className='welcome'>{startMessage}</p>
+          <p className='welcome'>{message}</p>
         </div>
 
       ) : (
 
         <div className='weather-data'>
-          <p className='city' >{locationKey[0].EnglishName}</p>
-          <p className='temperature' >{Math.round(weatherData[0].Temperature.Metric.Value)}°C</p>
-          <img src={require(`./images/${weatherData[0].WeatherIcon}.png`)} className='image' alt='Weather icon' />
-          <p className='weather-text' >{weatherData[0].WeatherText}</p>
+          <p className='city' >{weatherData.name}</p>
+          <p className='temperature' >{Math.round(weatherData.main.temp)}°C</p>
+          <img src={require (`./images/${weatherData.weather[0].icon}@2x.png`)} className='image' alt='Weather Icon' />
+          <p className='weather' >{weatherData.weather[0].main}</p>
 
         </div>
       )}
